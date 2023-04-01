d44bf0238ba5fdd57c42710f7dfb058904f02742
Téo cathelot <t.cathelot.pro@gmail.com>
2023-04-01 17:47:46 +0200

    PokeSecondCommit

---
+++ src/components/PokeCard.jsx
@@ -0,0 +1,13 @@
+function PokeId() {
+    return (
+        <figure>
+
+            <img className="card" src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'></img>
+            <figcaption className="card2">Bulbasaur</figcaption>
+        </figure>
+
+    )
+}
+
+
+export default PokeId;
