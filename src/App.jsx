95fa53b07b01431e205f1e6f9defb53691e1c148
Téo cathelot <t.cathelot.pro@gmail.com>
2023-04-01 17:41:14 +0200

    pokeCommit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PokeId from './components/PokeCard'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <PokeId />
+    </div>
+  )
+}
+
+export default App
